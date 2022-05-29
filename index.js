cc5c8db33685444964df669314f7b52bb24490f5
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-05-28 23:44:10 -0400

    added function and questionsArr

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function runQuiz() {
+   
+    var questionsArr = [
+        {
+            question:'Dogs are a woman/mans best friend',
+            answer: true
+        },
+        {
+            question:'Local coffee is superior to Starbucks',
+            answer: true
+        },
+        {
+            question: 'George Washington was the 10th President',
+            answer: false
+        },
+        {
+            question: '18 is the legal drinking age',
+            answer: false
+        },
+        {
+            question: 'Sunscreen can protect you from sun damage',
+            answer: true
+        },
+    ];
+   
+
+}
