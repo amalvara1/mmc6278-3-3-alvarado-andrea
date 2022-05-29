ae5ced9754645d7539e87f63980dd451f060566f
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-05-29 12:54:11 -0400

    added math.round and score alerts

--- index.js
+++ index.js
@@ -23,11 +23,21 @@
     ];
 
 function runQuiz() {
+
+    var correct = 0
     for (let i = 0; i < questionsArr.length; i++) {
         var question = questionsArr[i]
-        console.log(question)
         var answer = confirm(question)
+
+        if (answer) {
+            correct++
+        }
     }
-    
+
+    if(correct >= Math.round(questionsArr.length / 2)) {
+        alert("You got a 100%")
+    } else {
+        alert("You got a 0%")
+    }    
 
 }
